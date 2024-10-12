a8f819b81e35e39528cc23637a328d5b9a00f2be
Valerii Strilets <valerii.strilets@gmail.com>
2024-10-12 16:33:09 +0300

    init project

---
+++ nuxt.config.ts
@@ -0,0 +1,19 @@
+import Aura from "@primevue/themes/aura";
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: "2024-04-03",
+  future: {
+    compatibilityVersion: 4,
+  },
+  devtools: { enabled: true },
+  modules: ["@nuxtjs/tailwindcss", "@primevue/nuxt-module"],
+  css: ["primeicons/primeicons.css"],
+  primevue: {
+    options: {
+      theme: {
+        preset: Aura,
+      },
+    },
+  },
+});
